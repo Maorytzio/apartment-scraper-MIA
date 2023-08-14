d6b1f5458a27e82cfee6f58f3215d31367ea6559
Maor Beeri <marmaor@gmail.com>
2023-08-14 15:09:45 +0300

    added: links.py, requirements.txt

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import requests as requests
 from bs4 import BeautifulSoup
 import time
 
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
+from links import GOOGLE_FORM_LINK, ZILLOW_URL
 
 
 def init_driver():
@@ -17,21 +17,6 @@ def init_driver():
     drivr = webdriver.Chrome(options=options)
     return drivr
 
-
-GOOGLE_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSfn9kDZIbMzlKk-PVxW01SEqJA-S6KjYvLH4VHvWkj9vpWG2A/" \
-                   "viewform?usp=sf_link"
-
-LINK_FOR_TO_SHEET = "https://docs.google.com/forms/d/1KUrM_v9mHUZRewRZ3knaiO99qEAVToI2ABdGp6eJiek/edit#responses"
-
-ZILLOW_URL = "https://www.zillow.com/miami-fl/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds" \
-             "%22%3A%7B%22north%22%3A25.879527583425485%2C%22east%22%3A-80.07785136572265%2C%22south%22%3A25" \
-             ".526247572624758%2C%22west%22%3A-80.61000163427734%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22" \
-             "%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%2C%22max%22%3A1%7D%2C" \
-             "%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22" \
-             "%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22" \
-             "%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22" \
-             "%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A12700%2C" \
-             "%22regionType%22%3A6%7D%5D%2C%22usersSearchTerm%22%3A%22Miami%20FL%22%2C%22mapZoom%22%3A11%7D"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 "
@@ -73,7 +58,6 @@ for apt in results_list:
         all_links.append(f"https://www.zillow.com{pure_link}")
     else:
         all_links.append(pure_link)
-
 
 driver = init_driver()
 for i in range(len(all_links)):
